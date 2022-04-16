07f9225185451f104e554f0d6623e91d0ac4f487
Abdulrahman Abdullah <aabdullah014@gmail.com>
2022-04-16 14:57:59 -0700

    Works for multiple digits

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-next_num = 0
-current_num = 0
-display_value = 0
-operation = ""
+next_num = "0"
+current_num = "0"
+operation = "0"
+goToNextNum = false
 
 btn = document.querySelectorAll(".button");
 display = document.getElementById("answer");
+display.innerHTML = ""
 for(i = 0; i<btn.length; i++){
     // Aesthetic button color changes on mousedown and mouseup
     btn[i].addEventListener("mousedown", function onClick(event){ event.target.style.backgroundColor = "#A0A0F0"})
@@ -13,23 +14,28 @@ for(i = 0; i<btn.length; i++){
     // 
     btn[i].addEventListener("click", function onClick(event){
         if(event.target.innerHTML != "+" && event.target.innerHTML != "-" && event.target.innerHTML != "*" && event.target.innerHTML != "/" ){
-            display.innerHTML = event.target.innerHTML;
             if(event.target.innerHTML == "="){
+                console.log(current_num)
+                console.log(next_num)
                 res = operate(operation, current_num, next_num);
                 display.innerHTML = res
             } else if(event.target.innerHTML == "C"){
-                next_num = 0
-                current_num = 0
-                display.innerHTML = 0
+                next_num = "0"
+                current_num = "0"
+                display.innerHTML = ""
                 operation = ""
-                console.log(current_num)
-                console.log(next_num)
-                console.log(operation)
-            } else if(current_num != 0){
-                next_num = event.target.innerHTML;
+                goToNextNum = false
+            } else if(goToNextNum == true){
+                display.innerHTML+=event.target.innerHTML;
+                next_num+=event.target.innerHTML;
             }
-            else{current_num = event.target.innerHTML}
-        }else {operation = event.target.innerHTML}
+            else{
+                display.innerHTML+=event.target.innerHTML;
+                current_num += event.target.innerHTML}
+        }else {
+            goToNextNum = true
+            display.innerHTML = ""
+            operation = event.target.innerHTML}
 
     })
 }
